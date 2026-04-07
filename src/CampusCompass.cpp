e9a59128d994637e2467c00cf4b7f99a4c5e6797
gustavijavi <javier.collroman@gmail.com>
2026-04-07 13:16:53 -0400

    initializing all the files and creating template for parsing commands

--- src/CampusCompass.cpp
+++ src/CampusCompass.cpp
@@ -1,6 +1,7 @@
 #include "CampusCompass.h"
 
 #include <string>
+#include <regex>
 
 using namespace std;
 
@@ -14,9 +15,54 @@ bool CampusCompass::ParseCSV(const string &edges_filepath, const string &classes
 }
 
 bool CampusCompass::ParseCommand(const string &command) {
+
+    if (command.substr(0, 7) == "insert ") {
+        
+    } else if (command.substr(0, 7) == "remove ") {
+        
+    } else if (command.substr(0, 10) == "dropClass ") {
+        
+    } else if (command.substr(0, 13) == "replaceClass ") {
+        
+    } else if (command.substr(0, 12) == "removeClass ") {
+        
+    } else if (command.substr(0, 19) == "toggleEdgesClosure ") {
+        
+    } else if (command.substr(0, 16) == "checkEdgeStatus ") {
+        
+    } else if (command.substr(0, 12) == "isConnected ") {
+        
+    } else if (command.substr(0, 19) == "printShortestEdges "){
+
+    } else if (command.substr(0, 17) == "printStudentZone "){
+
+    } else if (command.substr(0, 15) == "verifySchedule ") {
+
+    } else {
+        // anything else unrecognized should be unsuccessful as the syntax is strict
+        return false;
+    }
+
+
     // do whatever regex you need to parse validity
     // hint: return a boolean for validation when testing. For example:
     bool is_valid = true; // replace with your actual validity checking
 
     return is_valid;
+}
+
+
+// helper function to see if the name has only numbers and is a length of 8
+bool isValidUfId(const string& ufId) {
+    if (ufId.length() != 8) {
+        return false;
+    }
+
+    for (char c : ufId) {
+        if (!isdigit(c)) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <string>
 
 #include "CampusCompass.h"
 
 using namespace std;
 
 int main() {
+
     // initialize your main project object
     CampusCompass compass;
 
     // ingest CSV data
     compass.ParseCSV("data/edges.csv", "data/classes.csv");
 
-    // the below is example code for parsing commandline input
-    int no_of_lines;
+    int numCommands;
     string command;
-    cin >> no_of_lines;
-    cin.ignore(); // ignore newline that first cin left over
-    for (int i = 0; i < no_of_lines; i++) {
+    cin >> numCommands;
+    cin.ignore(); // gets rid of the newline after the number is inputted
+
+    for (int i = 0; i < numCommands; i++) {
         getline(cin, command);
 
-        // parse your commands however you see fit
         compass.ParseCommand(command);
     }
+    return 0;
 }
